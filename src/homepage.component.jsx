b232fe5673cc84085f08395f1d47a7654727815c
molk95 <molk.saouabi@gmail.com>
2021-05-13 12:50:41 +0200

    first

---
+++ src/homepage.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HomePage = () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">CHAPEAU</h1>
+                    <span className="subtitle">ACHETER</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+)
+
+export default HomePage;
